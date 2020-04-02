87272863bcd36f0117b71fb660b4459b94a2b2c3
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-04-02 19:42:48 +0200

    ci: fix readme generation

--- scripts/build_readme.js
+++ scripts/build_readme.js
@@ -4,10 +4,13 @@ const { readFileSync, writeFileSync } = require("fs-extra");
 const { spawnSync } = require("child_process");
 const { join } = require("path");
 
-const commonHelp = spawnSync(join(__dirname, "../bin/intl-manager"), ["--help"]);
-const manageHelp = spawnSync(join(__dirname, "../bin/intl-manager"), ["manage", "--help"]);
+const BIN = join(__dirname, "../bin/locale-manager");
+const TEMPLATE = join(__dirname, "../README_template.md");
 
-let readme = readFileSync(join(__dirname, "../README_template.md"), "utf-8");
+const commonHelp = spawnSync(BIN, ["--help"]);
+const manageHelp = spawnSync(BIN, ["manage", "--help"]);
+
+let readme = readFileSync(TEMPLATE, "utf-8");
 
 readme = readme.replace("@TEMPLATE_COMMON_HELP", commonHelp.stdout.toString());
 readme = readme.replace("@TEMPLATE_MANAGE_HELP", manageHelp.stdout.toString());
