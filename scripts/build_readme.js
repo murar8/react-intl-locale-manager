ffb09a02f83f935f5dc4cd82eb1f29f5bfc6edbe
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-04-06 01:19:56 +0200

    docs: fix readme build script

--- scripts/build_readme.js
+++ scripts/build_readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 const { readFileSync, writeFileSync } = require("fs-extra");
-const { execSync } = require("child_process");
+const { execFileSync } = require("child_process");
 const { join } = require("path");
 
 const BIN = join(__dirname, "../bin/locale-manager");
 const TEMPLATE = join(__dirname, "../README_template.md");
 
-const help = execSync(BIN + " --help", { env: { COLUMNS: "80" }, stdio: "pipe" });
+const help = execFileSync(BIN, ["--help"], { env: { COLUMNS: "80" }, stdio: "pipe" });
 let readme = readFileSync(TEMPLATE, "utf-8");
 
 readme = readme.replace("@TEMPLATE_HELP", help.toString());
