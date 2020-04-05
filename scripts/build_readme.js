9e02f6925381a05661326dd534b38790c571b1a6
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-04-06 00:58:33 +0200

    docs(readme.md): wrap generated help

--- scripts/build_readme.js
+++ scripts/build_readme.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 const { readFileSync, writeFileSync } = require("fs-extra");
-const { spawnSync } = require("child_process");
+const { execSync } = require("child_process");
 const { join } = require("path");
 
 const BIN = join(__dirname, "../bin/locale-manager");
 const TEMPLATE = join(__dirname, "../README_template.md");
 
-const help = spawnSync(BIN, ["--help"]);
+const help = execSync(BIN + " --help", { env: { COLUMNS: "80" }, stdio: "pipe" });
 let readme = readFileSync(TEMPLATE, "utf-8");
 
-readme = readme.replace("@TEMPLATE_HELP", help.stdout.toString());
+readme = readme.replace("@TEMPLATE_HELP", help.toString());
 
 writeFileSync(join(__dirname, "../README.md"), readme);
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import yargs from "yargs";
 import { manageCommand } from "./command";
 
 export default () => {
-  yargs.detectLocale(false).strict().wrap(yargs.terminalWidth()).command(manageCommand).argv;
+  yargs
+    .detectLocale(false)
+    .strict()
+    .wrap(yargs.terminalWidth() || 80)
+    .command(manageCommand).argv;
 };
